18259cdda9e4d3b9414e89d4b6e91fe579b94c6d
Vikar007 <vikar_2000@email.ru>
2019-08-09 12:20:57 +0500

    20190809 2345

---
+++ src/IPAddress.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+import "./IPAddress.css";
+class IPAddress extends Component {
+    render() {
+        return (
+            <div>
+                <h1>{this.props.ip}</h1>
+                <p>(Это ваш IP-адрес... Наверное)</p>
+            </div>
+        );
+    }
+}
+export default IPAddress;
